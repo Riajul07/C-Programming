38533a35400edcdfe97f4d96d370c25189a44d52
reaz77 <reazahsan57@gmail.com>
2023-06-13 01:13:26 +0600

    Add comment

--- Basic/Add.c
+++ Basic/Add.c
@@ -1,8 +1,17 @@
+/*Addition, Subtraction, Multiplication and Division*/
+
 #include <stdio.h>
 int main(){
   int a, b, c;
   printf("Enter Your Two Number:\n");
   scanf("%d %d", &a, &b);
-  c = a + b;
+    c = a + b;
   printf(" The Sum of %d and %d is %d", a, b, c);
+    c = a - b;
+  printf(" The Sum of %d and %d is %d", a, b, c);
+    c = a * b;
+  printf(" The Sum of %d and %d is %d", a, b, c);
+    c = a / b;
+  printf(" The Sum of %d and %d is %d", a, b, c);
+  
 }
