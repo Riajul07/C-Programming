6e1bcae42899f35617608ff50d946790b0ce8d8f
raijul07 <riajahsan07@gmail.com>
2023-06-13 19:24:52 +0600

    unsolved

---
+++ Array/Minimum_numer_in_an_array.c
@@ -0,0 +1,24 @@
+/* output cannot count first input*/
+#include <stdio.h>
+int main(){
+    int array[100], minimum, size,i,location =1;
+    printf("Enter the number of elements of Array\n");
+    scanf("%d", &size);
+    printf("Enter %d interger\n", size);
+    for ( i = 0; i < size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    minimum = array[0];
+    for ( i = 0; i < size; i++)
+    {
+        if (array[i]< minimum)
+        {
+            minimum = array[i];
+            location = i+1;
+        }
+        
+    }
+    printf("Minimum element is is present %d and its value is %d", location, array[location]);
+    
+}
