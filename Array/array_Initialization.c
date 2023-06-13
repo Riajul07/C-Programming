c2df112e71584a918a21f924e95bfe2d0bacfd17
raijul07 <riajahsan07@gmail.com>
2023-06-13 15:25:08 +0600

    Create array_Initialization.c

---
+++ Array/array_Initialization.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int i;
+    int day[7]={1,2,3,4,5,6,7};
+    for ( i = 0; i < 7; i++)
+    {
+        printf("You are type \t  %d \n", day[i]);
+    }
+    return 0;
+}
